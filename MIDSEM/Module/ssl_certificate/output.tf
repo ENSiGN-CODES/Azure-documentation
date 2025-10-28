a7b67f4fddc1e0bc70d6815e4f886343ae19171d
Architpant <pantarchit11@gmail.com>
2025-10-28 09:19:12 +0530

    Delete MIDSEM/Module/ssl_certificate directory

--- MIDSEM/Module/ssl_certificate/output.tf
+++
@@ -1,9 +0,0 @@
-output "certificate_pem" {
-  description = "Generated PEM certificate"
-  value       = tls_self_signed_cert.cert.cert_pem
-}
-
-output "private_key_pem" {
-  description = "Generated Private Key"
-  value       = tls_private_key.private_key.private_key_pem
-}
